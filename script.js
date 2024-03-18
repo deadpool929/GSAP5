3386a262e9615cb74372b92f286ca50b8efe6858
Animesh Kumar Singh <74432497+deadpool929@users.noreply.github.com>
2024-03-19 01:27:46 +0530

    Added a loader to th website

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 let tl = gsap.timeline();
 
+function timer() {
+  var a = 0;
+  setInterval(() => {
+    a = a + Math.floor(Math.random() * 20);
+    if (a < 100) {
+      document.querySelector(".loader h1").innerHTML = a + "%";
+      console.log(a);
+    } else {
+      a = 100;
+      document.querySelector(".loader h1").innerHTML = a + "%";
+    }
+  }, 150);
+}
+
+tl.to(".loader", {
+  delay: 1,
+  duration: 0.5,
+  onStart: timer(),
+});
+
+tl.to(".loader", {
+  y: -1500,
+  delay: 0.5,
+  duration: 1,
+});
+
 tl.from(".nav ,.page1 h1,.page1 h3", {
   y: "-100",
 
@@ -14,7 +40,7 @@ tl.from(".page1 h2", {
 });
 
 tl.to(".page1 h2", {
-  transform: "translateX(-100%)",
+  transform: "translateX(-80%)",
   fontWeight: "100",
   scrollTrigger: {
     trigger: ".page1",
